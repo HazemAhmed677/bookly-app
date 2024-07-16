5b0a3260bb6470cdaea9501b8b9067875cc1b0a6
Hazem <hzm@gmail.com>
2024-07-16 23:10:04 +0300

    add all styles of app in style class

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff150025);
+const String kGtSectraFine = 'GT Sectra Fine';
+const String kMotserra = 'Montserrat';
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -6,11 +6,6 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Best Seller',
-        style: Styles.textStyle18,
-      ),
-    );
+    return SizedBox();
   }
 }
