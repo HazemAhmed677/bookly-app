e34bb18487278c6c47649190ae1a1d6247883111
Hazem <hzm@gmail.com>
2024-07-16 22:53:25 +0300

    add all missing fonts in pubspec

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,13 +1,12 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text(
         'Best Seller',
         style: Styles.textStyle18,
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -16,9 +15,6 @@ class BooklyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        textTheme: GoogleFonts.montserratAlternatesTextTheme(
-          ThemeData.dark().textTheme,
-        ),
         scaffoldBackgroundColor: kPrimaryColor,
       ),
       home: const SplashView(),
