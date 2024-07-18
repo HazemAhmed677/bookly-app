b6f471ab8bfe8016dc82b55386ba3b37dbee5006
Hazem <hzm@gmail.com>
2024-07-18 23:56:34 +0300

    create repo  layer of home

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+
+class Api extends HomeRepo {
+  @override
+  Future bestSellerBooks() {
+    // TODO: implement bestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
