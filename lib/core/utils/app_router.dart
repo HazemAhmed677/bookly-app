8d26925d63470c0fe1ce3189b775f9c1b27c0c44
Hazem <hzm@gmail.com>
2024-07-18 23:41:01 +0300

    refactor app router file

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,8 +1,8 @@
+import 'package:bookly_app/core/widgets/customm_transition_page.dart';
 import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -37,21 +37,4 @@ abstract class AppRouter {
       ),
     ),
   ]);
-}
-
-//--------------------------------------------------------------
-class CustommTransitionPage extends CustomTransitionPage<void> {
-  CustommTransitionPage(
-      {required LocalKey super.key,
-      required super.child,
-      required int duration})
-      : super(
-          transitionDuration: Duration(milliseconds: duration),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return FadeTransition(
-              opacity: animation,
-              child: child,
-            );
-          },
-        );
 }
