24bff2592b27e08485d462ac091b7e0c5c76d0e2
Hazem <hzm@gmail.com>
2024-07-18 21:11:51 +0300

    create text button action of book details

--- lib/features/home/presentation/views/book_details_view.dart
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -12,6 +12,7 @@ class BookDetailsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -22,7 +23,7 @@ class BookDetailsView extends StatelessWidget {
             ),
             Padding(
               padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.27,
+                horizontal: width * 0.27,
               ),
               child: const BookItem(),
             ),
@@ -51,8 +52,81 @@ class BookDetailsView extends StatelessWidget {
               height: 16,
             ),
             const CustomFeedBack(),
+            const SizedBox(
+              height: 35,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomActionButton(
+                    text: '19.99 €',
+                    background: Colors.white,
+                    textColor: Colors.black,
+                  ),
+                  CustomActionButton(
+                    text: 'Free preview',
+                    background: Color(0xffEF8262),
+                    textColor: Colors.white,
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CustomActionButton extends StatelessWidget {
+  const CustomActionButton({
+    super.key,
+    required this.background,
+    required this.textColor,
+    required this.text,
+  });
+  final String text;
+  final Color background;
+  final Color textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    bool flag = (background == Colors.white);
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: background,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: (flag) ? const Radius.circular(10) : Radius.zero,
+              bottomLeft: (flag) ? const Radius.circular(10) : Radius.zero,
+              topRight: (!flag) ? const Radius.circular(10) : Radius.zero,
+              bottomRight: (!flag) ? const Radius.circular(10) : Radius.zero,
+            ),
+          ),
+        ),
+        onPressed: () {},
+        child: (flag)
+            ? Text(
+                text,
+                style: Styles.textStyleForPrice.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: textColor,
+                ),
+              )
+            : Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Text(
+                  text,
+                  style: Styles.textStyle16.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: kGiloryMedium,
+                    color: textColor,
+                  ),
+                ),
+              ),
       ),
     );
   }
