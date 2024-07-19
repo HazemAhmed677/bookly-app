5a374e6925f6c2543709f9012f1be3920ff44a90
Hazem <hzm@gmail.com>
2024-07-19 20:58:07 +0300

    add fetch similar books cubit

--- lib/features/home/presentation/manager/fetch_similar_books_cubit/fetch_similar_books_cubit.dart
+++ lib/features/home/presentation/manager/fetch_similar_books_cubit/fetch_similar_books_cubit.dart
@@ -1,8 +1,24 @@
+// ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
 part 'fetch_similar_books_state.dart';
 
 class FetchSimilarBooksCubit extends Cubit<FetchSimilarBooksState> {
-  FetchSimilarBooksCubit() : super(FetchSimilarBooksInitial());
+  FetchSimilarBooksCubit({required this.homeRepoImpl})
+      : super(FetchSimilarBooksInitial());
+  final HomeRepoImpl homeRepoImpl;
+  Future<void> fetchSimilarBooks({required String category}) async {
+    emit(FetchSimilarBooksLoading());
+    var result = await homeRepoImpl.fetchSimilarBooks(category: category);
+    result.fold(
+      (failure) {
+        // will show  the error that I hundled in error folder
+        emit(FetchSimilarBooksFailuire(errorMsg: failure.errorMsg));
+      },
+      (listOfBooks) {
+        emit(FetchSimilarBooksSuccess(books: listOfBooks));
+      },
+    );
+  }
 }
--- lib/features/home/presentation/manager/fetch_similar_books_cubit/fetch_similar_books_state.dart
+++ lib/features/home/presentation/manager/fetch_similar_books_cubit/fetch_similar_books_state.dart
@@ -1,10 +1,19 @@
 part of 'fetch_similar_books_cubit.dart';
 
-sealed class FetchSimilarBooksState extends Equatable {
-  const FetchSimilarBooksState();
-
-  @override
-  List<Object> get props => [];
-}
+abstract class FetchSimilarBooksState {}
 
 final class FetchSimilarBooksInitial extends FetchSimilarBooksState {}
+
+final class FetchSimilarBooksLoading extends FetchSimilarBooksState {}
+
+final class FetchSimilarBooksSuccess extends FetchSimilarBooksState {
+  final List<BookModel> books;
+
+  FetchSimilarBooksSuccess({required this.books});
+}
+
+final class FetchSimilarBooksFailuire extends FetchSimilarBooksState {
+  final String errorMsg;
+
+  FetchSimilarBooksFailuire({required this.errorMsg});
+}
